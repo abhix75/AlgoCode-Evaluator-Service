f7f98e910b172e237b110491d01941d7da2b51d8
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-05-29 11:19:48 +0530

    Basic Ts + Express Project Setup Done

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express,{Express} from "express";
+import serverConfig from "./config/serverConfig";
+const app: Express = express();
+
+app.listen(3000,()=>{
+   
+    console.log(`Server started at PORT ${serverConfig.PORT}`)
+    
+})
